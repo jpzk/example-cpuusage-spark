19b2a46384cd75f0588bd034156d5791aad9c75f
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2016-03-14 13:05:48 +0100

    moved application conf; for assembly

--- src/main/scala/Config.scala
+++ src/main/scala/Config.scala
@@ -2,7 +2,7 @@ import java.io._
 import com.typesafe.config.ConfigFactory
 
 object Config {
-  val config = ConfigFactory.parseFile(new File("application.conf"))
+  val config = ConfigFactory.load()
   def CSVDirectory = config.getString("csv-directory")
   def OutDirectory = config.getString("output-directory")
 }
